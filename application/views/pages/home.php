19fd7328d32859ebfb49d08e87dab6b265a13843
rsyd29 <brasyid15@gmail.com>
2021-01-01 15:11:12 +0700

    ubah tampilan home

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,12 +1,25 @@
 <?php echo form_open('users/login'); ?>
-    <div class="row">
-        <div class="col-md-4 offset-md-4">
-            <h1 class="text-center">Sign in</h1>
-            <div class="form-group">
-                <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
-                <input type="password" name="password" class="form-control" placeholder="Password" required autofocus>
-            </div>
-            <button type="submit" class="btn btn-primary">Enter</button>
-        </div>
-    </div>
+
+<style>
+	a {
+		color: white;
+	}
+</style>
+
+<div class="row" style="margin: 50px auto;">
+	<div class="col-md-4 offset-md-4">
+		<br>
+		<h1 class="text-center" style="color: white">Sign In</h1>
+		<br>
+		<div class="form-group">
+			<input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+			<br>
+			<input type="password" name="password" class="form-control" placeholder="Password" required autofocus>
+		</div>
+		<button type="submit" class="btn btn-primary btn-block">Enter</button>
+		<a href="<?php echo base_url(); ?>users/register">
+			<center><u>Don't have an account?</center></u>
+		</a>
+	</div>
+</div>
 <?php echo form_close(); ?>
